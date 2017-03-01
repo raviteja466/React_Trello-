5c77df57b351e253a83853cb8f4b1d84ce7e8b03
Raviteja466 <raviteja.ravi143@gmail.com>
2017-03-01 14:48:03 -0500

    first commit

---
+++ src/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Board from './components/board';
+import './index.css';
+import 'bootstrap/dist/css/bootstrap.css';
+import 'bootstrap/dist/css/bootstrap-theme.css';
+
+ReactDOM.render(
+  <Board />,
+  document.getElementById('root')
+);
